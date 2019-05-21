4938fae6a974a2131e917e76d2d6c3d5fb9593df
Mark DeLuzio <deluziomark@gmail.com>
2019-05-21 15:55:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const listenNotesApiKey = '117c42b9078743dc89b2323493dc967d';
+
+const youtubeApiKey = 'AIzaSyDtfzdlcBrzEbC15WG2VRCvADNbTjDFlww';
+
+// Waits for the search form to be submitted
+function watchForm(){
+
+}
+
+// Makes call to YouTube API to request data based on user input
+function getYoutube() {
+    
+}
+
+// Displays the YouTube data from the API fetch request to the DOM
+function displayYoutube() {
+    
+}
+
+// Makes call to ListenNotes API to request data based on user input
+function getPodcast() {
+    
+}
+
+// Displays the ListenNotes data from the API fetch request to the DOM
+function displayPodcast() {
+
+}
+
+// Converts query parameters from an object to a string
+function convertToString() {
+
+}
+
+watchForm();
+
