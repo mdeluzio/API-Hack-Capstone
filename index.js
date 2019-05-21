c1966e469a5a7f2dc0dbaaf8df52735a0f4efe22
Mark DeLuzio <deluziomark@gmail.com>
2019-05-21 16:49:13 -0400

    Youtube API is linked

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-'use strict'
+//'use strict'
 
 const listenNotesApiKey = '117c42b9078743dc89b2323493dc967d';
 
 const youtubeApiKey = 'AIzaSyDtfzdlcBrzEbC15WG2VRCvADNbTjDFlww';
 
+const youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+
+
 // Waits for the search form to be submitted
 function watchForm(){
+    $('form').submit(function(event) {
+        event.preventDefault();
+        let searchTerm = $('.searchBox').val();
 
+        getYoutube(searchTerm);
+    })
+    
 }
 
 // Makes call to YouTube API to request data based on user input
-function getYoutube() {
-    
+function getYoutube(searchTerm, maxResults=25) {
+    const params = {
+        part: 'snippet',
+        q: searchTerm,
+        key: youtubeApiKey,
+        videoEmbeddable: 'true',
+        type: 'video',
+        maxResults
+    };
+
+    let queryString = convertToString(params);
+
+    let finalYoutubeUrl = youtubeUrl + '?' + queryString;
+
+    fetch(finalYoutubeUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } 
+            throw new Error(response.statusMessage);
+        })
+        .then(responseJson => displayYoutube(responseJson))
+        .catch(error => {
+            alert(`An error occured: ${response.statusMessage}`)
+        });    
 }
 
 // Displays the YouTube data from the API fetch request to the DOM
-function displayYoutube() {
-    
+function displayYoutube(responseJson) {
+    console.log(responseJson);
 }
 
 // Makes call to ListenNotes API to request data based on user input
@@ -30,9 +63,11 @@ function displayPodcast() {
 }
 
 // Converts query parameters from an object to a string
-function convertToString() {
-
+function convertToString(params) {
+    let queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
 }
 
-watchForm();
+$(watchForm);
 
